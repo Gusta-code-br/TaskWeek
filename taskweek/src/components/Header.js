c8e49d83c67f32b1523d1bef4aa2fdf8740ab14c
Gustavo Henrique Rodrigues da Cruz <120399245+Gusta-code-br@users.noreply.github.com>
2025-05-15 17:52:53 -0300

    Update Header.js

--- taskweek/src/components/Header.js
+++ taskweek/src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/components/Header.css';
+import logo from '../assets/images/logo-TaskWeek.png';
 
 function Header() {
   return (
     <header className="header">
-      <h1>TaskWeek</h1>
+      <img id="imagemlogo" src={logo} width='200' alt="Logo Taskweek" className="logo" />
+      <h1 className="title">Bem vindo ao seu sistema de controle de atividades! Task Week!</h1>
     </header>
   );
 }
