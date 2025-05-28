48b019a77b713317d925b6a2387764882a19cf26
Gustavo <gustavohenrique92rv@gmail.com>
2025-05-28 20:06:10 -0300

    🐛

--- taskweek_backend/models/Task.js
+++ taskweek_backend/models/Task.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const sequelize = require('../config/database'); // Sem destructuring
 
 const Task = sequelize.define('Task', {
   id: {
